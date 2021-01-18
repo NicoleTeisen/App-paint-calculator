1a7ae702a688bcaa44a870825b248a15e7556c68
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2021-01-18 08:06:23 -0300

    criando funções para os campos de incluir portas e janelas seguindo as regras de negócio

--- src/components/WallMeasure.js
+++ src/components/WallMeasure.js
@@ -1,23 +1,34 @@
 import React, { Component } from "react";
 
-class Wall1 extends Component {
+class WallMeasure extends Component {
   constructor(props) {
     super(props);
 
-    this.updateDataWidth = this.updateDataWidth.bind(this);    
-    this.updateArea = this.updateArea.bind(this);
+    this.updateDataWidth = this.updateDataWidth.bind(this);
     this.updateDataHeight = this.updateDataHeight.bind(this);
+    this.updateArea = this.updateArea.bind(this);
+    this.checkChangeDoor = this.checkChangeDoor.bind(this);
+    this.changeValueSelectDoor = this.changeValueSelectDoor.bind(this);
+    this.checkChangeWindows = this.checkChangeWindows.bind(this);
+    this.doorArea = this.doorArea.bind(this);
+    this.windowsArea = this.windowsArea.bind(this);
+    this.frameArea = this.frameArea.bind(this);
+    this.compareAreas = this.compareAreas.bind(this);
 
     this.state = {
       width: 0,
       height: 0,
       wallArea: 0,
+      usefulArea: 0,
       disableHeight: true,
       disableDoor: true,
-      checkDoor: false,      
-      usefulArea: 0,
+      checkDoor: false,
+      doorArea: 0,
+      disableWindows: true,
+      checkWindows: false,
+      windowsArea: 0,
+      frameArea: 0,
     };
-
   }
 
   updateArea() {
@@ -45,7 +56,7 @@ class Wall1 extends Component {
           },
           this.updateArea
         );
-  } 
+  }
 
   updateDataHeight(key, value) {
     const { checkDoor } = this.state;
@@ -67,22 +78,144 @@ class Wall1 extends Component {
           this.updateArea
         );
   }
-  
+
+  changeValueSelectDoor() {
+    const door = document.getElementById("door-select");
+    door.value = 0;
+    console.log(door.value);
+    this.frameArea();
+  }
+
+  checkChangeDoor() {
+    const { height, checkDoor } = this.state;
+    height < 2.2
+      ? this.setState(
+          {
+            checkDoor: false,
+          },
+          alert(
+            "A altura da parede deve ser de no mínimo 2,20m para habilitar este campo"
+          )
+        )
+      : this.setState(
+          checkDoor === false
+            ? {
+                disableDoor: false,
+                checkDoor: true,
+              }
+            : {
+                disableDoor: true,
+                checkDoor: false,
+                doorArea: 0,
+              },
+          this.changeValueSelectDoor
+        );
+  }
+
+  doorArea(key, value) {
+    const area = 0.8 * 1.9;
+    const totalArea = area * value;
+    this.setState(
+      {
+        [key]: totalArea,
+      },
+      this.frameArea
+    );
+  }
+
+  changeValueSelectWindows() {
+    const windows = document.getElementById("windows-select");
+    windows.value = 0;
+    console.log(windows.value);
+    this.frameArea();
+  }
+
+  checkChangeWindows() {
+    const { checkWindows } = this.state;
+    this.setState(
+      checkWindows === false
+        ? {
+            disableWindows: false,
+            checkWindows: true,
+          }
+        : {
+            disableWindows: true,
+            checkWindows: false,
+            windowsArea: 0,
+          },
+      this.changeValueSelectWindows
+    );
+  }
+
+  windowsArea(key, value) {
+    const area = 2 * 1.2;
+    const totalArea = area * value;
+    this.setState(
+      {
+        [key]: totalArea,
+      },
+      this.frameArea
+    );
+  }
+
+  frameArea() {
+    const { doorArea, windowsArea } = this.state;
+    console.log(windowsArea, doorArea);
+    const sum = doorArea + windowsArea;
+    console.log(sum);
+    this.setState(
+      {
+        frameArea: sum,
+      },
+      this.compareAreas
+    );
+  }
+
+  compareAreas() {
+    const { wallArea, frameArea } = this.state;
+    const halfArea = wallArea / 2;
+    console.log(wallArea, frameArea, halfArea);
+    if (frameArea > halfArea) {
+      const windows = document.getElementById("windows-select");
+      windows.value = 0;
+      const door = document.getElementById("door-select");
+      door.value = 0;
+      alert(
+        "Verifique a quantidade de esquadrias, a sua área não pode ultrapassar 50% da área total da parede"
+      );
+      this.setState(
+        {
+          windowsArea: 0,
+          doorArea: 0,
+        },
+        this.frameArea
+      );
+    } else {
+      const totalArea = wallArea - frameArea;
+      this.setState({
+        usefulArea: totalArea,
+      });
+    }
+  }
 
   render() {
-    const {           
+    const {
+      disableHeight,
+      disableDoor,
       height,
-      disableHeight, 
-      checkDoor,      
+      checkDoor,
+      disableWindows,
+      checkWindows,
     } = this.state;
 
     return (
       <div>
         <h3>Parede 1:</h3>
-        <form>
+        <form className="wall">
           <label>
             Largura
             <input
+              className="input-text"
               placeholder="0"
               type="number"
               step={0.1}
@@ -97,6 +230,7 @@ class Wall1 extends Component {
           <label>
             Altura
             <input
+              className="input-text"
               value={height}
               type="number"
               step={0.1}
@@ -107,48 +241,61 @@ class Wall1 extends Component {
             metros
           </label>
 
-          <label>
-            <input
-              type="checkbox"
-              name="door-check"  
-              checked={checkDoor}            
-            />
-            Essa parede possui porta(s)
-          </label>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                name="door-check"
+                checked={checkDoor}
+                onClick={this.checkChangeDoor}
+              />
+              Essa parede possui porta(s)
+            </label>
 
-          <select
-            id="door-select"            
-          >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+            <select
+              id="door-select"
+              className="select"
+              disabled={disableDoor}
+              onChange={(e) => this.doorArea("doorArea", e.target.value)}
+            >
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
 
-          <label>
-            <input
-              type="checkbox"
-              name="windows-check"              
-            />
-            Essa parede possui janela(s)
-          </label>
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                name="windows-check"
+                checked={checkWindows}
+                onClick={this.checkChangeWindows}
+              />
+              Essa parede possui janela(s)
+            </label>
 
-          <select
-            id="windows-select"            
-          >
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
+            <select
+              id="windows-select"
+              className="select"
+              disabled={disableWindows}
+              onChange={(e) => this.windowsArea("windowsArea", e.target.value)}
+            >
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
         </form>
       </div>
     );
   }
 }
 
-export default Wall1;
+export default WallMeasure;
--- src/pages/Data.js
+++ src/pages/Data.js
@@ -4,7 +4,7 @@ import WallMeasure from "../components/WallMeasure";
 class Data extends Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1 data-testid="settings-title">CALCULADORA DE TINTA</h1>
         <WallMeasure />
         
